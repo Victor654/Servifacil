a9a8e007fb92ffbc71465de3f3a65a481bcb221a
victor <naranjohoyosvictormanuel@gmail.com>
2020-05-07 18:00:55 -0500

    git commit

--- application/Model/Tests/Logintest.php
+++ application/Model/Tests/Logintest.php
@@ -2,7 +2,7 @@
 //namespace Mini\Model;
 //use Mini\Model\user;
 //use Mini\Core\Model;
-//require_once 'user.php';
+//require 'user.php';
 use user;
 use PHPUnit\Framework\TestCase;
 
@@ -12,9 +12,9 @@ class LoginTest extends TestCase
     /** @test */
     public function testLogin()
     {
-        $user = new User();
-        $this->assertEquals(1007311489,$user -> userExists());
-        $this->assertEquals(1567,$user -> userExists());
+        $Usuario = new User();
+        $this->assertEquals(1007311489,$Usuario -> userExists());
+        $this->assertEquals(1567,$Usuario -> userExists());
         $this->assertTrue(true);
     }
 }
