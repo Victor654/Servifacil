a57e2094ca3d67db89116f37094ad9beec1bd60e
Victor <naranjohoyosvictormanuel@gmail.com>
2020-05-20 08:15:42 -0500

    git commit

--- application/tests/Logintest.php
+++ application/tests/Logintest.php
@@ -1,9 +1,9 @@
 <?php
-//namespace Mini\Tests;
-//use Mini\Model\user;
+namespace Mini\tests;
+use Mini\Model\user;
 use PHPUnit\Framework\TestCase;
 //use PHPUnit\DbUnit\TestCaseTrait;
-require_once('application\Model\user.php');
+//require_once('application\Model\user.php');
 //use user;
 class Logintest extends  TestCase
 {
@@ -11,8 +11,8 @@ class Logintest extends  TestCase
     {
        // $userForm = 1007311489;
         //$passForm = 1567;
-        $user = new User($userForm, $passFom);
-        $subject = user::userExists(1007311489,1567);
+        $user = new User();
+        $this->assertEquals(userExists(1007311489,1567));
         $this->assertTrue(true,$subject);
     }
 
